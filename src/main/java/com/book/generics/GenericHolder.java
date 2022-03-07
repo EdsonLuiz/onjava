33882f2b042d19e4ad87f7b513113a5317bdb46c
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-03-07 06:59:44 -0300

    feat: generic holder

---
+++ src/main/java/com/book/generics/GenericHolder.java
@@ -0,0 +1,16 @@
+package com.book.generics;
+
+public class GenericHolder<T> {
+  private T a;
+  public GenericHolder() {}
+
+  public void set(T a) {this.a = a;}
+  public T get() {return a;}
+
+  public static void main(String[] args) {
+    GenericHolder<Automobile> h3 = new GenericHolder<>();
+    
+    h3.set(new Automobile());
+    Automobile a = h3.get();
+  }
+}
