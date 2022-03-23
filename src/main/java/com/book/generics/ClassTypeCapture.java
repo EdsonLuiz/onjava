31aa19d8d8eb9105e8770b677266e2efbb44842a
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-03-23 07:15:55 -0300

    :sparkles: ClassTypeCapture (IMPORTANT)

---
+++ src/main/java/com/book/generics/ClassTypeCapture.java
@@ -0,0 +1,28 @@
+package com.book.generics;
+
+public class ClassTypeCapture<T> {
+  
+  Class<T> kind;
+
+  public ClassTypeCapture(Class<T> kind) {
+    this.kind = kind;
+  }
+
+  public boolean f(Object arg) {
+    return kind.isInstance(arg);
+  }
+
+  public static void main(String[] args) {
+    ClassTypeCapture<Building> ctt1 = new ClassTypeCapture<>(Building.class);
+    System.out.println(ctt1.f(new Building())); // Building isInstance of Building
+    System.out.println(ctt1.f(new House()));    // Building isInstance of House
+
+    ClassTypeCapture<House> ctt2 = new ClassTypeCapture<>(House.class);
+    System.out.println(ctt2.f(new Building())); // House isInstance of Building
+    System.out.println(ctt2.f(new House()));    // House isInstance of House
+
+  }
+}
+
+class Building {}
+class House extends Building {}
