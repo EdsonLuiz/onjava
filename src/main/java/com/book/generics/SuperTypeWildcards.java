1f068e3c9c617ae68e13cb4985dce7d24d57f0c0
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-04-01 07:46:48 -0300

    :sparkles: SuperTypeWildcards

---
+++ src/main/java/com/book/generics/SuperTypeWildcards.java
@@ -0,0 +1,12 @@
+package com.book.generics;
+
+import java.util.List;
+
+
+public class SuperTypeWildcards {
+  static void writeTo(List<? super Apple> apples) {
+    apples.add(new Apple());
+    apples.add(new Jonathan());
+    // apples.add(new Fruit());   // Error
+  }
+}
