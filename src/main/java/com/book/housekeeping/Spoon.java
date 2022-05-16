d5a8fcff0df22d2429c5bbc8e3a5a047519afe3e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-05-16 06:56:24 -0300

    :sparkles: Spoon

---
+++ src/main/java/com/book/housekeeping/Spoon.java
@@ -0,0 +1,13 @@
+package com.book.housekeeping;
+
+/**
+ * You can group other static initializations inside a special
+ * "static clouse"
+ */
+public class Spoon {
+  static int i;
+
+  static {
+    i = 47;
+  }
+}
