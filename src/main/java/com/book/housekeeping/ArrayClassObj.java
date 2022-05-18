22f2ff1b103f4f870f7426899f24a5ade4c0a480
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-05-18 06:57:36 -0300

    :sparkles: ArrayClassObj

---
+++ src/main/java/com/book/housekeeping/ArrayClassObj.java
@@ -0,0 +1,19 @@
+package com.book.housekeeping;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayClassObj {
+  public static void main(String[] args) {
+    Random rand = new Random(47);
+    Integer[] a = new Integer[rand.nextInt(20)];
+
+    System.out.println("Length of a: " + a.length);
+
+    for(int i = 0; i < a.length; i++) {
+      a[i] = rand.nextInt(500); //   autoboxing int to Integer
+    }
+
+    System.out.println(Arrays.toString(a));
+  }
+}
