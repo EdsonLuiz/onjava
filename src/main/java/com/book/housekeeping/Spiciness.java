013a03280ab7e496d6df042915f91e279eac7258
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-05-20 07:24:13 -0300

    :sparkles: Spiciness: Enum

---
+++ src/main/java/com/book/housekeeping/Spiciness.java
@@ -0,0 +1,9 @@
+package com.book.housekeeping;
+
+/**
+ * Instances of enumerate types are constants.
+ * by convention they are in all capital letters.
+ */
+public enum Spiciness {
+  NOT, MILD, MEDIUM, HOT, FLAMING
+}
