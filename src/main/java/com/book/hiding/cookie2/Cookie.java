d804b28f8a56baa22cafb0428b338a08761a9b8e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-05-30 07:16:08 -0300

    :sparkles: Cookie bite() use protected and becomes accessible to anyone inheriting from Cookie

---
+++ src/main/java/com/book/hiding/cookie2/Cookie.java
@@ -0,0 +1,11 @@
+package com.book.hiding.cookie2;
+
+public class Cookie {
+  public Cookie() {
+    System.out.println("Cookie constructor");
+  }
+
+  protected void bite() {
+    System.out.println("bite");
+  }
+}
