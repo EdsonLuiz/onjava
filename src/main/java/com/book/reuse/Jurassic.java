1c645ff0dc992dd8c642a20da8a9f4283448ce2f
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-06-15 07:56:23 -0300

    :sparkles: Jurassic
    Making an entire class final

---
+++ src/main/java/com/book/reuse/Jurassic.java
@@ -0,0 +1,24 @@
+// Making an entire class final
+
+package com.book.reuse;
+
+class SmallBrain {}
+
+final class Dinosaur {
+  int i = 7;
+  int j = 1;
+  SmallBrain x = new SmallBrain();
+  void f() {}
+}
+
+// class Further extends Dinosaur {}
+// error: Cannot extends final class 'Dinosaur'
+
+public class Jurassic {
+  public static void main(String[] args) {
+    Dinosaur n = new Dinosaur();
+    n.f();
+    n.i  = 40;
+    n.j++;
+  }
+}
