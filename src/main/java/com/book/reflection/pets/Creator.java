09e9c523dc52e235864f184fef0a58b48e5644a1
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-07-14 07:55:48 -0300

    :sparkles: Creator

--- src/main/java/com/book/reflection/pets/Creator.java
+++ src/main/java/com/book/reflection/pets/Creator.java
@@ -1,16 +1,41 @@
 package com.book.reflection.pets;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public abstract class Creator implements Supplier<T> {
+public abstract class Creator implements Supplier<Pet> {
   private Random rand = new Random(47);
 
   // The different types of Pet to create
-  public abstract List<Class<?> extends Pet>
-}
+  public abstract List<Class<? extends Pet>> types();
 
-public class Creator {
-  
+  @Override
+  public Pet get() {
+    int n = rand.nextInt(types().size());
+    try {
+      return types().get(n).getConstructor().newInstance();
+    } catch ( InstantiationException |
+              NoSuchMethodException |
+              InvocationTargetException |
+              IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public Stream<Pet> strean() {
+    return Stream.generate(this);
+  }
+
+  public Pet[] array(int size) {
+    return strean().limit(size).toArray(Pet[]::new);
+  }
+
+  public List<Pet> list(int size) {
+    return strean().limit(size).collect(Collectors.toCollection(ArrayList::new));
+  }
 }
