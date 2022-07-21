6e4b8dcb8d4ce029708a6a4bff9bdef5e9a81bba
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-07-21 07:56:28 -0300

    :sparkles: RegisteredFactories

---
+++ src/main/java/com/book/reflection/RegisteredFactories.java
@@ -0,0 +1,95 @@
+package com.book.reflection;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+class Part implements Supplier<Part> {
+  @Override
+  public String toString() {
+    return getClass().getSimpleName();
+  }
+
+  static List<Supplier<? extends Part>> prototypes = Arrays.asList(
+    new FuelFilter(),
+    new AirFilter(),
+    new CabinAirFilter(),
+    new OilFilter(),
+    new FanBelt(),
+    new PowesSteeringBelt(),
+    new GeneratorBelt()
+  );
+
+  private static Random rand = new Random(47);
+
+  @Override
+  public Part get() {
+    int n = rand.nextInt(prototypes.size());
+    return prototypes.get(n).get();
+  }
+}
+
+class Filter extends Part {}
+
+class FuelFilter extends Filter {
+  @Override
+  public FuelFilter get() {
+    return new FuelFilter();
+  }
+}
+
+class AirFilter extends Filter {
+  @Override
+  public AirFilter get() {
+    return new AirFilter();
+  }
+}
+
+class CabinAirFilter extends Filter {
+  @Override
+  public CabinAirFilter get() {
+    return new  CabinAirFilter();
+  }
+}
+
+class OilFilter extends Filter {
+  @Override
+  public OilFilter get() {
+    return new OilFilter();
+  }
+}
+
+class Belt extends Part {}
+
+class FanBelt extends Belt {
+  @Override
+  public FanBelt get() {
+    return new FanBelt();
+  }
+}
+
+class GeneratorBelt extends Belt {
+  @Override
+  public GeneratorBelt get() {
+    return new GeneratorBelt();
+  }
+}
+
+class PowesSteeringBelt extends Belt {
+  @Override
+  public PowesSteeringBelt get() {
+    return new PowesSteeringBelt();
+  }
+}
+
+
+
+public class RegisteredFactories {
+  public static void main(String[] args) {
+    Stream.generate(new Part())
+      .limit(10)
+      .forEach(System.out::println);
+  }
+}
