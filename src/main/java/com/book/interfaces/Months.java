ca02fc39c1af4882666965cee119740cd6e71cbd
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-08-19 07:32:33 -0300

    :sparkles: Months
    Fields in interfaces are automatically static, final and public

---
+++ src/main/java/com/book/interfaces/Months.java
@@ -0,0 +1,13 @@
+package com.book.interfaces;
+
+/*
+ * Fields in interfaces are automatically static and final.
+ * pre-Java 5
+ * Fields are automatically public.
+ */
+public interface Months {
+  int JANUARY = 1, FEBRUARY = 2, MARCH = 3,
+      APRIL = 4, MAY = 5, JUNE = 6, JULY = 7,
+      AUGUST = 8, SEPTEMBER = 9, OCTOBER = 10,
+      NOVEMBER = 11, DECEMBER = 12;
+}
