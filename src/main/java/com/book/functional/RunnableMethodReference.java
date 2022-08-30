e0b922d42fd5b3c4297f68736d9a3ce9f2f840c0
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-08-30 07:11:21 -0300

    :sparkles: RunnableMethodReference

---
+++ src/main/java/com/book/functional/RunnableMethodReference.java
@@ -0,0 +1,23 @@
+package com.book.functional;
+
+class Go {
+  static void go() {
+    System.out.println("Go::go()");
+  }
+}
+
+public class RunnableMethodReference {
+  public static void main(String[] args) {
+    new Thread(new Runnable() {
+      public void run() {
+        System.out.println("Anonymous");
+      }
+    }).start();
+
+    new Thread(
+      () -> System.out.println("lambda")
+    ).start();
+
+    new Thread(Go::go).start();
+  }
+}
