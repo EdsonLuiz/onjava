97bcf0330fa349dca5bce75d36f716428ca2d55b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-09-12 06:44:10 -0300

    :sparkles: FunctionVariants

--- src/main/java/com/book/functional/FunctionVariants.java
+++ src/main/java/com/book/functional/FunctionVariants.java
@@ -9,6 +9,7 @@ import java.util.function.IntToDoubleFunction;
 import java.util.function.IntToLongFunction;
 import java.util.function.LongFunction;
 import java.util.function.LongToDoubleFunction;
+import java.util.function.LongToIntFunction;
 import java.util.function.ToDoubleFunction;
 import java.util.function.ToIntFunction;
 import java.util.function.ToLongFunction;
@@ -51,8 +52,27 @@ public class FunctionVariants {
   static ToDoubleFunction<DBaz> f7 = db -> db.d;
   static IntToLongFunction f8 = i -> i;
   static IntToDoubleFunction f9 = i -> i;
-  static IntToLongFunction f10 = l -> (int)l;
+  static LongToIntFunction f10 = l -> (int)l;
   static LongToDoubleFunction f11 = l -> l;
   static DoubleToIntFunction f12 = d -> (int)d;
   static DoubleToLongFunction f13 = d -> (long)d;
+
+  public static void main(String[] args) {
+    Bar b = f1.apply(new Foo());
+    IBaz ib = f2.apply(11);
+    LBazz lb = f3.apply(11);
+    DBaz db = f4.apply(11);
+
+    int i = f5.applyAsInt(ib);
+    long l = f6.applyAsLong(lb);
+    double d = f7.applyAsDouble(db);
+
+    l = f8.applyAsLong(12);
+    d = f9.applyAsDouble(12);
+    i = f10.applyAsInt(12);
+    d = f11.applyAsDouble(12);
+    i = f12.applyAsInt(13);
+    l = f13.applyAsLong(13.0);
+
+  }
 }
