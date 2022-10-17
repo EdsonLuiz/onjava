6a8f73f1d0b0b327e3eb87bfc4e48817c1e8a9c0
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-10-17 06:46:19 -0300

    :sparkles: SelectElement

---
+++ src/main/java/com/book/streams/SelectElement.java
@@ -0,0 +1,12 @@
+package com.book.streams;
+
+public class SelectElement {
+  public static void main(String[] args) {
+    System.out.println(RandInts.rands().findFirst().getAsInt());
+    System.out.println(
+      RandInts.rands().parallel().findFirst().getAsInt()
+    );
+    System.out.println(RandInts.rands().findAny().getAsInt());
+    System.out.println(RandInts.rands().parallel().findAny().getAsInt());
+  }
+}
