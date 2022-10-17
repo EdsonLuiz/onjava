ca3ef8d61a0c0e6a56997bab18c51714b0a5c835
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2022-10-17 07:00:58 -0300

    :sparkles: LastElement

---
+++ src/main/java/com/book/streams/LastElement.java
@@ -0,0 +1,19 @@
+package com.book.streams;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class LastElement {
+  public static void main(String[] args) {
+    OptionalInt last = IntStream.range(10, 20)
+      .reduce((n1, n2) -> n2);
+    System.out.println(last.orElse(-1));
+
+    // Non-numeric object:
+    Optional<String> lastobj = Stream.of("one", "two", "three")
+      .reduce((o1, o2) -> o2);
+    System.out.println(lastobj.orElse("Nothing there"));
+  }
+}
